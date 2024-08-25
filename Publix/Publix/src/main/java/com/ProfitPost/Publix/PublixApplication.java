fde0f6b4bea4086a70fee2d9feaa333c2564e4b9
Hernan Mauriccio Gaston Berrospi Reyes <mauriccioreyes@gmail.com>
2024-08-25 18:53:58 -0500

    Estructura Base del Proyecto

---
+++ Publix/Publix/src/main/java/com/ProfitPost/Publix/PublixApplication.java
@@ -0,0 +1,13 @@
+package com.ProfitPost.Publix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublixApplication.class, args);
+	}
+
+}
---
+++ Publix/Publix/src/test/java/com/ProfitPost/Publix/PublixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProfitPost.Publix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
